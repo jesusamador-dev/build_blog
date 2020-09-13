a72ec052f05075cc38d518341635512a11fc0da1
Jesús Guadalupe Macías Amador <jesus.macias.amador@gmail.com>
2020-09-12 22:14:24 -0500

    Cambiando cors para pruebas

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
-const corsOptions = { origin: "http://admin.jesusamador.com" };
+// const cors = require('cors');
+// const axios = require('axios');
+// const corsOptions = { origin: "http://admin.jesusamador.com" };
 
 const webHooks = (app) => {
     const router = express.Router();
-    app.use(cors(corsOptions));
+    // app.use(cors(corsOptions));
     app.use('/api/webhooks', router);
 
     router.post('/published', (req, res, next) => {
